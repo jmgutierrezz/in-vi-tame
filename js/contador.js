d6f8607d34dc77f5e5d87519b7c8e22bc74b4587
José Miguel Gutiérrez Medina <jmgutierrez0811@gmail.com>
2021-02-24 12:04:58 -0430

    Agregando el tiempo del evento

--- js/contador.js
+++ js/contador.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
-    year: 2020, // required
-    month: 2, // required
-    day: 21, // required
-    hours: 1, // Default is 0 [0-23] integer
+    year: 2021, // required
+    month: 3, // required
+    day: 15, // required
+    hours: 12, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
