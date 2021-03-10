4c33e32ea59989b11d64211c0dd7e54317a54aca
José Miguel Gutiérrez Medina <jmgutierrez0811@gmail.com>
2021-03-10 11:11:47 -0430

    Cambiar fecha a 8 nov

--- invitaciones/js/contador.js
+++ invitaciones/js/contador.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
     year: 2021, // required
-    month: 3, // required
-    day: 15, // required
+    month: 11, // required
+    day: 8, // required
     hours: 12, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
